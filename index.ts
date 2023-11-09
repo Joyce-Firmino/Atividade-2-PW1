e84308aa401233028811914bc042441ea4c042de
joyce <joyce069.js@gmail.com>
2023-11-09 20:07:40 -0300

    Criando o metodo post para usuario e tecnologia

--- index.ts
+++ index.ts
@@ -22,6 +22,54 @@ async function retornaUsuarioExistente(req: Request, res: Response, next: NextFu
     } 
 }
 
+
+
+//criando um novo usuario
+app.post('/users', async (req,res) => {
+    const {nome, usernome} = req.body 
+    const comparaUser= await prismaClient.usuario.findFirst({
+        where: {
+            nome: nome,
+        }
+    })
+    if(comparaUser !== null){
+       return res.status(400).json({error: "Usuário já existe cadastre um novo usuário!"})
+    }
+    const novoClient = await prismaClient.usuario.create({
+        data:{
+            nome,
+            usernome,
+            tecnologias: {
+                create: []
+            }
+        }
+       });
+    return res.status(201).json(novoClient)
+})
+
+
+
+// Criando uma nova tecnologia
+app.post('/tecnologias', retornaUsuarioExistente, async (req, res) => {
+    try {
+        const { titulo, dtPrazoFinal } = req.body;
+        const novaTecnologia = await prismaClient.tecnologia.create({
+            data: {
+                titulo,
+                marcarEstudado: false,
+                dtPrazoFinal: new Date(dtPrazoFinal).toISOString(),
+                usuarioId: req.userExpr.id
+            }
+        });
+
+        return res.status(201).json(novaTecnologia);
+    } catch (error) {
+        return res.status(400).json({ error: "Tecnologia não foi criada" });
+    }
+});
+
+
+
 app.listen(3002, () => {
     console.log("conectado");
 })
